2953f58f0b4f1e86b804e598534cead458985c36
Priyanshu Chauhan <12411priyanshuchauhan@gmail.com>
2023-08-07 17:26:56 -0400

    Basic Authentication

---
+++ index.js
@@ -0,0 +1,20 @@
+const connectToMongo=require('./db');
+const express = require('express')
+const cors = require('cors')
+
+connectToMongo();
+
+
+const app = express()
+app.use(cors())
+const port = 5000
+app.use(express.json())
+
+// Available Routes
+
+app.use('/api/auth',require('./routes/auth'))
+
+
+app.listen(port, () => {
+  console.log(`Template backend app listening on port http://localhost:${port}`)
+})
